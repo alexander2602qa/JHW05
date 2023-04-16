a84d207ceba171f0d25f8430964edd2e110fcbf6
Alexander <alex.albasov@gmail.com>
2023-04-16 14:39:46 +0700

    task 2 has been done

--- src/test/java/ru/netology/services/TimeOfRestServiceTest.java
+++ src/test/java/ru/netology/services/TimeOfRestServiceTest.java
@@ -2,35 +2,20 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class TimeOfRestServiceTest {
 
-    @Test
-    public void testWithValidDate1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/TimeOfRestDataset.csv")
+    public void testWithValidDate(int income, int expenses, int threshold, int expected) {
         TimeOfRestService service = new TimeOfRestService();
-
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
 
         int actual = service.calcTimeOfRest(income, expenses, threshold);
 
-        Assertions.assertEquals(expected,actual);
-    }
-
-    @Test
-    public void testWithValidDate2() {
-        TimeOfRestService service = new TimeOfRestService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        int actual = service.calcTimeOfRest(income, expenses, threshold);
-
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
 }
